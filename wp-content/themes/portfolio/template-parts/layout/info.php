197afe7d1afcd980ea5da2c22ce93f9fc51f7f9d
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2023-07-31 20:22:14 +0200

    information on desktop

--- wp-content/themes/portfolio/template-parts/layout/info.php
+++ wp-content/themes/portfolio/template-parts/layout/info.php
@@ -26,16 +26,50 @@ $anchor = pods_field('infoanchor', $id);
         </h2>
       </div>
       <div class="content-wrap basis-3/4">
-        <p class="w-4/5">
+        <p>
           <?php echo $text; ?>
         </p>
-        <div class="info-elements">
-          <div class="info-element">
+        <div class="info-elements flex flex-row justify-between mt-24 flex-wrap gap-6">
+          <div class="info-element flex flex-row items-center gap-4">
             <div class="svg-wrap rounded-full w-20 h-20 flex justify-center items-center">
-              <svg height="26">
-                <use xlink:href="#bday"></use>
+              <svg class="fill-white" height="30" width="30">
+                <use xlink:href="#school"></use>
               </svg>
             </div>
+            <?php
+            $birthDate = "12/19/2001";
+            $birthDate = explode("/", $birthDate);
+            $age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
+              ? ((date("Y") - $birthDate[2]) - 1)
+              : (date("Y") - $birthDate[2]));
+            echo $age . " Jahre alt";
+            ?>
+          </div>
+
+          <div class="info-element flex flex-row items-center gap-4">
+            <div class="svg-wrap rounded-full w-20 h-20 flex justify-center items-center">
+              <svg class="fill-white" height="30" width="30">
+                <use xlink:href="#school"></use>
+              </svg>
+            </div>
+            Brilon, NRW
+          </div>
+          <div class="info-element flex flex-row items-center gap-4">
+            <div class="svg-wrap rounded-full w-20 h-20 flex justify-center items-center">
+              <svg class="fill-white" height="30" width="30">
+                <use xlink:href="#school"></use>
+              </svg>
+            </div>
+            Allg. Hochschulreife (2020)
+          </div>
+          <div class="info-element flex flex-row items-center gap-4">
+            <div class="svg-wrap rounded-full w-20 h-20 flex justify-center items-center">
+              <svg class="fill-white" height="30" width="30">
+                <use xlink:href="#school"></use>
+              </svg>
+            </div>
+            Abgeschlossene Ausbildung<br>
+            zum Mediengestalter Digital
           </div>
         </div>
       </div>
